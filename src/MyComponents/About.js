78ed75bca997ee464046ea1567b256df24c2b24b
Siddhant Priyadarshi <siddhantp4ever@gmail.com>
2021-07-18 00:56:23 +0530

    First Commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is an about component 
+            <p>
+                This is a ToDo app build by me. My first React js project.
+            </p>
+        </div>
+    )
+}
